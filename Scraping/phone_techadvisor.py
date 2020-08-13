aa4ff6b21f21ff691cef9ad044fbe18649459446
PhuongAnhMannheim <aenhchen@web.de>
2020-08-13 13:01:38 +0200

    corrected techadvisor

--- Scraping/phone_techadvisor.py
+++ Scraping/phone_techadvisor.py
@@ -43,7 +43,6 @@ for page in range(1, 69):
     reviews = soup.find_all('li', class_='media')
     for review in reviews:
         links.append(review.a['href'])
-print(len(links))
 
 host = "https://www.techadvisor.co.uk/"
 review_count = 0
@@ -56,8 +55,8 @@ extract_count = 0
 for review_link in links:
     review_count += 1
     if review_link in current_reviews:
-        already_count = 0
-        pass
+        already_count += 1
+        continue
     else:
         response = get(review_link)
         soup = BeautifulSoup(response.text, 'lxml')
@@ -65,22 +64,23 @@ for review_link in links:
         node = generateNode(31)
         url = review_link
         reviewText = review.find('div', itemprop='reviewBody')
-        try:
-            reviewText.find('section').decompose()
-        except:
+        if reviewText.find('section') is None:
             pass
-            reviewBody = reviewText.text.strip()
-            try:
-                ratingValue = review.find('meta', itemprop='ratingValue')['content']
-                bestRating = review.find('meta', itemprop='bestRating')['content']
-                worstRating = review.find('meta', itemprop='worstRating')['content']
-                reviewRating = ''
-                c.execute(
-                    f"INSERT OR IGNORE INTO {db_name} (NODE, URL, REVIEWBODY, RATING, REVIEWRATING, BESTRATING, WORSTRATING) VALUES (?,?,?,?,?,?,?);",
-                    (node, review_link, reviewBody, str(reviewRating), ratingValue, bestRating, worstRating))
-                conn.commit()
-                extract_count += 1
-            except:
-                no_rating += 1
+        else:
+            reviewText.find('section').decompose()
+        reviewBody = reviewText.text.strip()
+        try:
+            ratingValue = review.find('meta', itemprop='ratingValue')['content']
+            bestRating = review.find('meta', itemprop='bestRating')['content']
+            worstRating = review.find('meta', itemprop='worstRating')['content']
+            reviewRating = ''
+            c.execute(
+                f"INSERT OR IGNORE INTO {db_name} (NODE, URL, REVIEWBODY, RATING, REVIEWRATING, BESTRATING, WORSTRATING) VALUES (?,?,?,?,?,?,?);",
+                (node, review_link, reviewBody, str(reviewRating), ratingValue, bestRating, worstRating))
+            conn.commit()
+            extract_count += 1
+        except:
+            no_rating += 1
 logging.debug(f"Done {host} - Reviews extracted: " + str(extract_count) + " out of " + str(review_count))
 logging.debug("without Rating: " + str(no_rating) + ", problems: " + str(problem_count))
+logging.debug("Already in: " + str(already_count))
