d1efd5b396290b11b5eb3bfcf8626c74f3ccc472
PhuongAnhMannheim <aenhchen@web.de>
2020-08-13 14:07:47 +0200

    tested coolsmartphone

--- Scraping/phone_coolsmartphone.py
+++ Scraping/phone_coolsmartphone.py
@@ -10,9 +10,9 @@ current_file = 'already_links_in/coolsmartphone.txt'
 current_reviews = set(line.strip() for line in open(current_file))
 
 # Output
-db_path = '../Data/phonereviews.db'
-db_name = 'phonereviews'
-log_path = '../Logs/phoneReviews.log'
+db_path = '../Data/test.db'
+db_name = 'test'
+log_path = '../Logs/test.log'
 conn = sqlite3.connect(db_path)
 c = conn.cursor()
 
