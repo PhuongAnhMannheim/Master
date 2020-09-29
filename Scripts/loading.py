028ea8712b30b49b3fd142acd515cc6018b5529d
PhuongAnhMannheim <aenhchen@web.de>
2020-09-30 00:06:32 +0200

    redo cleaning Amazon data

--- Scripts/loading.py
+++ Scripts/loading.py
@@ -21,9 +21,8 @@ def load_schema_full(input_link):
 
 
 def load_amazon_full(input_link):
-    input_file = '../Data/reviews_Movies_and_TV_5.json.gz'
     data = []
-    with gzip.open(input_file) as file:
+    with gzip.open(input_link) as file:
         for line in file:
             data.append(json.loads(line.strip()))
 
@@ -34,50 +33,144 @@ def load_amazon_full(input_link):
 
 
 def load_merged_data(schema_link, amazon_link, schema_per_class, amazon_per_class):
-    schema_df = load_schema_full(schema_link)
-    schema_df_1 = schema_df[schema_df['text'] == 1.0].values.tolist()
-    schema_df_2 = schema_df[schema_df['text'] == 2.0].values.tolist()
-    schema_df_3 = schema_df[schema_df['text'] == 3.0].values.tolist()
-    schema_df_4 = schema_df[schema_df['text'] == 4.0].values.tolist()
-    schema_df_5 = schema_df[schema_df['text'] == 5.0].values.tolist()
+    schema_df = pd.read_pickle(schema_link)
+    schema_df_1 = schema_df[schema_df['label'] == 1.0].values.tolist()
+    schema_df_2 = schema_df[schema_df['label'] == 2.0].values.tolist()
+    schema_df_3 = schema_df[schema_df['label'] == 3.0].values.tolist()
+    schema_df_4 = schema_df[schema_df['label'] == 4.0].values.tolist()
+    schema_df_5 = schema_df[schema_df['label'] == 5.0].values.tolist()
 
     try:
-        random.sead(123)
+        random.seed(123)
         df1 = random.sample(schema_df_1, schema_per_class)
-    except:
-        random.sead(123)
-        df1 = random.choices(schema_df_1, schema_per_class)
+    except ValueError:
+        random.seed(123)
+        df1 = random.choices(schema_df_1, k=schema_per_class)
     try:
-        random.sead(123)
+        random.seed(123)
         df2 = random.sample(schema_df_2, schema_per_class)
-    except:
-        random.sead(123)
-        df2 = random.choices(schema_df_2, schema_per_class)
+    except ValueError:
+        random.seed(123)
+        df2 = random.choices(schema_df_2, k=schema_per_class)
     try:
-        random.sead(123)
+        random.seed(123)
         df3 = random.sample(schema_df_3, schema_per_class)
-    except:
-        random.sead(123)
-        df3 = random.choices(schema_df_3, schema_per_class)
+    except ValueError:
+        random.seed(123)
+        df3 = random.choices(schema_df_3, k=schema_per_class)
     try:
-        random.sead(123)
+        random.seed(123)
         df4 = random.sample(schema_df_4, schema_per_class)
-    except:
-        random.sead(123)
-        df4 = random.choices(schema_df_4, schema_per_class)
+    except ValueError:
+        random.seed(123)
+        df4 = random.choices(schema_df_4, k=schema_per_class)
     try:
-        random.sead(123)
+        random.seed(123)
         df5 = random.sample(schema_df_5, schema_per_class)
-    except:
-        random.sead(123)
-        df5 = random.choices(schema_df_5, schema_per_class)
+    except ValueError:
+        random.seed(123)
+        df5 = random.choices(schema_df_5, k=schema_per_class)
     df11 = pd.DataFrame(df1)
     df12 = pd.DataFrame(df2)
     df13 = pd.DataFrame(df3)
     df14 = pd.DataFrame(df4)
     df15 = pd.DataFrame(df5)
     schema = pd.concat([df11, df12, df13, df14, df15])
-    schema.reset_index(drop=True)
+    ama_df = pd.read_pickle(amazon_link)
+    ama_df_1 = ama_df[ama_df['label'] == 1.0].values.tolist()
+    ama_df_2 = ama_df[ama_df['label'] == 2.0].values.tolist()
+    ama_df_3 = ama_df[ama_df['label'] == 3.0].values.tolist()
+    ama_df_4 = ama_df[ama_df['label'] == 4.0].values.tolist()
+    ama_df_5 = ama_df[ama_df['label'] == 5.0].values.tolist()
 
+    try:
+        random.seed(123)
+        adf1 = random.sample(ama_df_1, amazon_per_class)
+    except ValueError:
+        random.seed(123)
+        adf1 = random.choices(ama_df_1, k=amazon_per_class)
+    try:
+        random.seed(123)
+        adf2 = random.sample(ama_df_2, amazon_per_class)
+    except ValueError:
+        random.seed(123)
+        adf2 = random.choices(ama_df_2, k=amazon_per_class)
+    try:
+        random.seed(123)
+        adf3 = random.sample(ama_df_3, amazon_per_class)
+    except ValueError:
+        random.seed(123)
+        adf3 = random.choices(ama_df_3, k=amazon_per_class)
+    try:
+        random.seed(123)
+        adf4 = random.sample(ama_df_4, amazon_per_class)
+    except ValueError:
+        random.seed(123)
+        adf4 = random.choices(ama_df_4, k=amazon_per_class)
+    try:
+        random.seed(123)
+        adf5 = random.sample(ama_df_5, amazon_per_class)
+    except ValueError:
+        random.seed(123)
+        adf5 = random.choices(ama_df_5, k=amazon_per_class)
+    adf11 = pd.DataFrame(adf1)
+    adf12 = pd.DataFrame(adf2)
+    adf13 = pd.DataFrame(adf3)
+    adf14 = pd.DataFrame(adf4)
+    adf15 = pd.DataFrame(adf5)
+    amazon = pd.concat([adf11, adf12, adf13, adf14, adf15])
+    df_all = pd.concat([schema, amazon], ignore_index=True)
+    df_all = df_all[[0,1]]
+    df_all.columns = ['text', 'label']
+    return df_all
+
+
+def load_sampled_amazon(amazon_link, amazon_per_class):
+    ama_df = pd.read_pickle(amazon_link)
+    ama_df_1 = ama_df[ama_df['label'] == 1.0].values.tolist()
+    ama_df_2 = ama_df[ama_df['label'] == 2.0].values.tolist()
+    ama_df_3 = ama_df[ama_df['label'] == 3.0].values.tolist()
+    ama_df_4 = ama_df[ama_df['label'] == 4.0].values.tolist()
+    ama_df_5 = ama_df[ama_df['label'] == 5.0].values.tolist()
+
+    try:
+        random.seed(123)
+        adf1 = random.sample(ama_df_1, amazon_per_class)
+    except ValueError:
+        random.seed(123)
+        adf1 = random.choices(ama_df_1, k=amazon_per_class)
+    try:
+        random.seed(123)
+        adf2 = random.sample(ama_df_2, amazon_per_class)
+    except ValueError:
+        random.seed(123)
+        adf2 = random.choices(ama_df_2, k=amazon_per_class)
+    try:
+        random.seed(123)
+        adf3 = random.sample(ama_df_3, amazon_per_class)
+    except ValueError:
+        random.seed(123)
+        adf3 = random.choices(ama_df_3, k=amazon_per_class)
+    try:
+        random.seed(123)
+        adf4 = random.sample(ama_df_4, amazon_per_class)
+    except ValueError:
+        random.seed(123)
+        adf4 = random.choices(ama_df_4, k=amazon_per_class)
+    try:
+        random.seed(123)
+        adf5 = random.sample(ama_df_5, amazon_per_class)
+    except ValueError:
+        random.seed(123)
+        adf5 = random.choices(ama_df_5, k=amazon_per_class)
+    adf11 = pd.DataFrame(adf1)
+    adf12 = pd.DataFrame(adf2)
+    adf13 = pd.DataFrame(adf3)
+    adf14 = pd.DataFrame(adf4)
+    adf15 = pd.DataFrame(adf5)
+    amazon = pd.concat([adf11, adf12, adf13, adf14, adf15], ignore_index=True)
+    df_all = amazon[[0, 1]]
+    df_all.columns = ['text', 'label']
+    return df_all
 
 
--- Scripts/profiling.py
+++ Scripts/profiling.py
@@ -3,6 +3,19 @@ import numpy as np
 import plotly.graph_objects as go
 
 
+def create_word_count(df):
+    df['word_count'] = df.text.apply(lambda x: len(str(x).split(" ")))
+    return df
+
+
+def get_missing_text(df):
+    return df[df.text.isnull()]
+
+
+def get_missing_label(df):
+    return df[df.label.isnull()]
+
+
 def get_review_count(df):
     print("Amount of reviews: ", len(df))
 
@@ -11,23 +24,27 @@ def get_descr(df):
     print(df.describe(include='all'))
 
 
-def create_word_count(df):
-    df['word_count'] = df.text.apply(lambda x: len(str(x).split(" ")))
-    return df
-
-
 def get_longest_review(df):
     longest_t = df[df['word_count'] == max(df['word_count'])]
     print('The longest review text in our sample has {} words.'.format(max(df['word_count'])))
     print('Longest review text:' + '\n')
-    print(longest_t.text)
+    print(longest_t.text, longest_t.label)
 
 
 def get_shortest_review(df):
     shortest_t = df[df['word_count'] == min(df['word_count'])]
     print('The shortest review text in our sample has {} words.'.format(min(df['word_count'])))
     print('Review text with the shortest length of {} word appearing {} times.'.format(min(df['word_count']), len(shortest_t)))
-    print(shortest_t.text)
+    print(shortest_t.text, shortest_t.label)
+
+
+def get_word_length_dist(df, dataname, log):
+    plt.rcParams['figure.facecolor'] = 'white'
+    plt.hist(df['word_count'].value_counts().sort_index(), range=(0, 6000), bins=100, log=log)
+    plt.xlabel('Text length')
+    plt.ylabel('Distribution')
+    plt.title(f'Text lengths in words ({dataname})')
+    df['word_count'].describe()
 
 
 def show_word_length_dist(df, filename, dataname, log):
@@ -42,12 +59,13 @@ def show_word_length_dist(df, filename, dataname, log):
     df['word_count'].describe()
 
 
-def show_rating_dist(df, filename):
+def show_rating_dist(df, filename, title):
     # e.g. amazon_cell_class_dist_raw
     print(df.label.value_counts())
     plt.rcParams['figure.facecolor'] = 'white'
     plt.xlabel('Class')
     plt.ylabel('Amount')
+    plt.title(f'Rating distribution ({title})')
     plt.hist(df.label, bins=np.arange(0.5, 6), rwidth=0.5)
     plt.savefig(f'../Figures/{filename}.png')
 
@@ -63,7 +81,7 @@ def show_word_length_per_label(df):
         dplot.append(trace)
 
     layout = go.Layout(
-        height=1000,
+        height=900,
         width=1200,
         title="Review Text - Length Distribution by Label",
         margin=dict(l=30, r=20, b=150, t=90)
