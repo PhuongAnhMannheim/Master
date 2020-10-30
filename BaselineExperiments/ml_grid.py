0be7ba2c582c6195844ed8a6b51300450ac9eb56
PhuongAnhMannheim <aenhchen@web.de>
2020-10-30 10:47:12 +0100

    correct ml_grid

--- BaselineExperiments/ml_grid.py
+++ BaselineExperiments/ml_grid.py
@@ -9,60 +9,9 @@ from sklearn.svm import SVC, SVR
 
 amazon_link = '../Data/amazon_phone.pkl'
 
-# ToDo: reference to module
-# from Scripts import loading as dl
-# df = dl.load_sampled(amazon_link, 5000)
-def load_sampled(link, per_class):
-    df = pd.read_pickle(link)
-    df_1 = df[df['label'] == 1.0].values.tolist()
-    df_2 = df[df['label'] == 2.0].values.tolist()
-    df_3 = df[df['label'] == 3.0].values.tolist()
-    df_4 = df[df['label'] == 4.0].values.tolist()
-    df_5 = df[df['label'] == 5.0].values.tolist()
 
-    try:
-        random.seed(123)
-        adf1 = random.sample(df_1, per_class)
-    except ValueError:
-        random.seed(123)
-        adf1 = random.choices(df_1, k=per_class)
-    try:
-        random.seed(123)
-        adf2 = random.sample(df_2, per_class)
-    except ValueError:
-        random.seed(123)
-        adf2 = random.choices(df_2, k=per_class)
-    try:
-        random.seed(123)
-        adf3 = random.sample(df_3, per_class)
-    except ValueError:
-        random.seed(123)
-        adf3 = random.choices(df_3, k=per_class)
-    try:
-        random.seed(123)
-        adf4 = random.sample(df_4, per_class)
-    except ValueError:
-        random.seed(123)
-        adf4 = random.choices(df_4, k=per_class)
-    try:
-        random.seed(123)
-        adf5 = random.sample(df_5, per_class)
-    except ValueError:
-        random.seed(123)
-        adf5 = random.choices(df_5, k=per_class)
-    adf11 = pd.DataFrame(adf1)
-    adf12 = pd.DataFrame(adf2)
-    adf13 = pd.DataFrame(adf3)
-    adf14 = pd.DataFrame(adf4)
-    adf15 = pd.DataFrame(adf5)
-    df_all = pd.concat([adf11, adf12, adf13, adf14, adf15], ignore_index=True)
-    df_all = df_all[[2, 1]]
-    df_all.columns = ['text_prep', 'label']
-    print(f'{per_class} reviews per class from {link} loaded')
-    return df_all
-
-
-df = load_sampled(amazon_link, 5000)
+from Scripts import loading as dl
+df = dl.load_sampled(amazon_link, 5000)
 target = df.label
 text = df.text_prep
 
